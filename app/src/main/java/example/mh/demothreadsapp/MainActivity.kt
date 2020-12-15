31a4e826786d4112f38bf2eff0e8b6fb8daae3cf
Maciek <maciej.heintze96@gmail.com>
2020-12-15 17:04:17 +0100

    get current location commit

--- app/src/main/java/example/mh/demothreadsapp/MainActivity.kt
+++ app/src/main/java/example/mh/demothreadsapp/MainActivity.kt
@@ -1,11 +1,76 @@
 package example.mh.demothreadsapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
 
+const val LOC_REQUEST_CODE = 80
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var lastLocation: Location? = null
+    private lateinit var saveLocation: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        onStartButtonPressed()
+    }
+
+    private fun onStartButtonPressed(){
+        start_button_id.setOnClickListener {
+            getCurrentLocationAsText()
+        }
+    }
+
+    private fun getCurrentLocationAsText(){
+        getCurrentLocation()
+        val lat = lastLocation?.latitude.toString()
+        val lon = lastLocation?.longitude.toString()
+        saveLocation = "$lat, $lon"
+    }
+
+    private fun getCurrentLocation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    LOC_REQUEST_CODE)
+            } else {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        lastLocation = location
+                    }
+            }
+        } else {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    lastLocation = location
+                }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOC_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        lastLocation = location
+                    }
+            } else {
+                Toast.makeText(this, "Location permission denied!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
